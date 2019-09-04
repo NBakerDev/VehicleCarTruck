dbe058f2fe1d4c4f1696f95d548be863a4debdd8
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-09-04 09:17:53 -0400

    Add project files.

---
+++ VehicleCarTruckLibrary/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace VehicleCarTruckLibrary {
+    public class Class1 {
+    }
+}
---
+++ VehicleCarandtruck/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VehicleCarandtruck {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
